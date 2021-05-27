ca3cd6e2dc28d0c357702e87f59d927e7b142e8b
ivaylokenov <ivaylo.kenov@gmail.com>
2021-05-27 21:34:03 +0300

    Added a feature to stop the request if it is too large

--- MyWebServer.Server/HttpServer.cs
+++ MyWebServer.Server/HttpServer.cs
@@ -51,11 +51,20 @@
             var bufferLength = 1024;
             var buffer = new byte[bufferLength];
 
+            var totalBytes = 0;
+
             var requestBuilder = new StringBuilder();
 
             while (networkStream.DataAvailable)
             {
                 var bytesRead = await networkStream.ReadAsync(buffer, 0, bufferLength);
+
+                totalBytes += bytesRead;
+
+                if (totalBytes > 10 * 1024)
+                {
+                    throw new InvalidOperationException("Request is too large.");
+                }
 
                 requestBuilder.Append(Encoding.UTF8.GetString(buffer, 0, bytesRead));
             }
