06fa01d3cbc41489b4a7f03533afb8418a7f1499
Berat-Dzhevdetov <56674380+Berat-Dzhevdetov@users.noreply.github.com>
2021-06-07 20:51:44 +0300

    Return View
    I made a return view method in base Controller that looks for a folder, that should be named as the controller itself, then gets the file with the same name as the method
    Code for the base Controller: https://pastebin.com/RxMjVVmz
    Architecture should look like this:
    https://user-images.githubusercontent.com/56674380/120883816-00b6da00-c5e8-11eb-82f1-4451d342ad08.png
    Usage:
    https://cdn.discordapp.com/attachments/652893707594498069/851518735885795328/unknown.png

--- MyWebServer/Controllers/Controller.cs
+++ MyWebServer/Controllers/Controller.cs
@@ -1,22 +1,60 @@
-﻿namespace MyWebServer.Controllers
+namespace CSharpWebServer.Server.Controllers
 {
-    using MyWebServer.Http;
-    using MyWebServer.Responses;
+    using System;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Reflection;
+    using CSharpWebServer.Server.Http;
+    using CSharpWebServer.Server.Responses;
 
     public abstract class Controller
     {
-        protected Controller(HttpRequest request)
-            => this.Request = request;
-
         protected HttpRequest Request { get; private init; }
+
+        protected Controller(HttpRequest request)
+        => this.Request = request;
 
         protected HttpResponse Text(string text)
             => new TextResponse(text);
 
-        protected HttpResponse Html(string html)
-            => new HtmlResponse(html);
+        protected HttpResponse Html(string text)
+            => new HtmlResponse(text);
 
         protected HttpResponse Redirect(string location)
             => new RedirectResponse(location);
+
+        protected HttpResponse View()
+        {
+            StackTrace trace = new StackTrace();
+            StackFrame frame = trace.GetFrame(1); // 0 will be the inner-most method
+            MethodBase method = frame.GetMethod();
+            var controllerName = GetNameOfController(method.DeclaringType.Name).Trim();
+            if (controllerName == null) new NotFoundResponse();
+
+            var methodName = trace.GetFrame(1).GetMethod().Name.Trim();
+
+            string codeBase = Assembly.GetExecutingAssembly().Location;
+            UriBuilder uri = new UriBuilder(codeBase);
+            string path = Uri.UnescapeDataString(uri.Path);
+            var dirName =  Path.GetDirectoryName(path);
+
+            var pathToFile = Path.GetFullPath(Path.Combine(dirName,@"..\..\..\","Views",controllerName));
+
+            if (!Directory.Exists(pathToFile)) return new NotFoundResponse();
+
+            var filePath = Directory.GetFiles(pathToFile, $"{methodName.ToLower()}.cshtml")[0];
+            var html = File.ReadAllText(filePath);
+            return new HtmlResponse(html);
+        }
+        private string GetNameOfController(string fullname)
+        {
+            if (fullname.Length <= 10)
+            {
+                return null;
+            }
+            var indexOfControllerWord = fullname.IndexOf("Controller");
+            return fullname.Substring(0, indexOfControllerWord);
+        }
     }
 }
