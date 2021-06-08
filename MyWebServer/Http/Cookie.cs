937b2826d99a8ce6cd1a55c1ba7126220cd0b24c
Vasil Stoyanov <63401488+vasilstoyanov99@users.noreply.github.com>
2021-06-08 19:33:23 +0300

    Used Guard function in the Cookie class constructor

--- MyWebServer/Http/Cookie.cs
+++ MyWebServer/Http/Cookie.cs
@@ -1,15 +1,22 @@
 ﻿namespace MyWebServer.Http
 {
+    using MyWebServer.Common;
+
     public class Cookie
     {
         public Cookie(string name, string value)
         {
+            Guard.AgainstNull(name, nameof(name));
+            Guard.AgainstNull(value, nameof(value));
+
             this.Name = name;
             this.Value = value;
         }
 
         public Cookie(string cookieAsString)
         {
+            Guard.AgainstNull(cookieAsString, nameof(cookieAsString));
+
             var cookieParts = cookieAsString.Split(new [] { '=' }, 2);
             this.Name = cookieParts[0];
             this.Value = cookieParts[1];
